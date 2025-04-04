eac1412f7e360360c4def91771b31a5756868fe7
LeoSan <cuenca623@gmail.com>
2025-04-04 14:50:47 -0600

    update

---
+++ Primeros_pasos_Python/CursoPython/Practicas/Basicas/02_IO_.py
@@ -0,0 +1,14 @@
+import io
+
+
+name = input("Ingrese su nombre: ")
+age = input("Ingrese su edad: ")
+
+print(  type(name))
+print( type(age))
+
+
+##print( "La variable nombre es de tipo -> " + type(name))
+##print("La variable edad es de tipo -> " +type(age))
+
+print(f" Tu edad ingresas es: {age} , Tu nombre Ingresado es: {name} ")
